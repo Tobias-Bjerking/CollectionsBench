f45ebb2e03b1ee630e06c06a2f5601a07b4f9a63
Michel Tosu <michel@tosu.se>
2020-03-02 11:31:50 +0100

    feature: add command line arguments for iterations with defaults

--- BenchmarkRunner.py
+++ BenchmarkRunner.py
@@ -10,16 +10,22 @@ timestamp = time.strftime(timeformat)
 base_path = timestamp + "_Benchmark"
 os.mkdir(base_path)
 
+# No default values for the implementation and benchmark variables
+num_arguments = len(sys.argv)
+if num_arguments < 3:
+    print "You need to supply the following arguments: \n benchmarks implementations [warmup_iterations iterations]\n"
+    sys.exit()
+
 implementations = sys.argv[2]
 benchmarks = sys.argv[1]
 
-warmup_iterations = 5
-iterations = 10
+warmup_iterations = sys.argv[3] if num_arguments > 3 and sys.argv[4] is not None else 20
+iterations = sys.argv[4] if num_arguments > 4 and sys.argv[4] is not None else 40
 
 for i in range(1,5):
 	threads = 2**i
-	file_path = base_path + "/" + "testRun" + str(i) + ".csv"
-	bashCommand = "java -jar benchmarks.jar {5} -p impl={4} -rff {0} -r 5 -i {1} -wi {2} -t {3} -bm thrpt".format(file_path, iterations, warmup_iterations, threads, implementations, benchmarks)
+	file_path = base_path + "/bench_" + benchmarks + "-threads_" + str(threads) + "-datastruct_" + implementations + ".csv"
+	bashCommand = "java -jar benchmarks.jar .*{5}.* -p impl={4} -rff {0} -r 5 -i {1} -wi {2} -t {3} -bm thrpt".format(file_path, iterations, warmup_iterations, threads, implementations, benchmarks)
 
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	while True:
