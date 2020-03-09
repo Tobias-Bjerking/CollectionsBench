92243eea582608e200b08d112154054a34cff7c4
Tobbe <tobias.bjerking@gmail.com>
2020-03-09 11:00:22 +0100

    Update benchmarkrunner and gitignore

--- BenchmarkRunner.py
+++ BenchmarkRunner.py
@@ -13,19 +13,20 @@ os.mkdir(base_path)
 # No default values for the implementation and benchmark variables
 num_arguments = len(sys.argv)
 if num_arguments < 3:
-    print "You need to supply the following arguments: \n benchmarks implementations [warmup_iterations iterations]\n"
+    print "You need to supply the following arguments: \n benchmarks implementations test_type [warmup_iterations iterations]\n"
     sys.exit()
 
 implementations = sys.argv[2]
 benchmarks = sys.argv[1]
+test_type = sys.argv[3]
 
-warmup_iterations = sys.argv[3] if num_arguments > 3 and sys.argv[4] is not None else 20
-iterations = sys.argv[4] if num_arguments > 4 and sys.argv[4] is not None else 40
+warmup_iterations = sys.argv[4] if num_arguments > 3 and sys.argv[4] is not None else 20
+iterations = sys.argv[5] if num_arguments > 4 and sys.argv[5] is not None else 40
 
 for i in range(1,5):
 	threads = 2**i
 	file_path = base_path + "/bench_" + benchmarks + "-threads_" + str(threads) + "-datastruct_" + implementations + ".csv"
-	bashCommand = "java -jar benchmarks.jar .*{5}.* -p impl={4} -rff {0} -r 5 -i {1} -wi {2} -t {3} -bm thrpt".format(file_path, iterations, warmup_iterations, threads, implementations, benchmarks)
+	bashCommand = "java -jar benchmarks.jar .*{5}.* -p impl={4} -p testType={6}  -rff {0} -r 5 -i {1} -wi {2} -t {3} -bm thrpt" .format(file_path, iterations, warmup_iterations, threads, implementations, benchmarks, test_type)
 
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	while True:
