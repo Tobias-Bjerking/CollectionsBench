97ff0bc824e28ab61f468ebcfb0a9b8064de4334
Tobbe <tobias.bjerking@gmail.com>
2020-03-10 12:59:22 +0100

    script now runs seperate test for the different distributions

--- BenchmarkRunner.py
+++ BenchmarkRunner.py
@@ -14,27 +14,25 @@ os.mkdir(base_path)
 num_arguments = len(sys.argv)
 if num_arguments < 3:
     print "You need to supply the following arguments: \n benchmarks implementations test_type [warmup_iterations iterations]\n"
-    sys.exit()
 
-implementations = sys.argv[2]
-benchmarks = sys.argv[1]
-test_type = sys.argv[3]
+benchmarks = sys.argv[1] if num_arguments > 1 else 'Online'
+implementations = ('-p impl=' + sys.argv[2]) if num_arguments > 2 else ''
+warmup_iterations = sys.argv[3] if num_arguments > 3 else 20
+iterations = sys.argv[4] if num_arguments > 4 else 40
 
-warmup_iterations = sys.argv[4] if num_arguments > 3 and sys.argv[4] is not None else 20
-iterations = sys.argv[5] if num_arguments > 4 and sys.argv[5] is not None else 40
+for t in ['even', 'update', 'iterate']:
+	for i in range(1, 4):
+		threads = 2**i
+		file_path = base_path + "/bench_" + benchmarks + '-testType_' + t + ".csv"
+		bashCommand = "java -jar benchmarks.jar .*{4}.* {3} -p testType={5}  -rff {0} -r 5 -i {1} -wi {2} -t {6} -bm thrpt".format(file_path, iterations, warmup_iterations, implementations, benchmarks, t, threads)
 
-for i in range(1,5):
-	threads = 2**i
-	file_path = base_path + "/bench_" + benchmarks + "-threads_" + str(threads) + "-datastruct_" + implementations + ".csv"
-	bashCommand = "java -jar benchmarks.jar .*{5}.* -p impl={4} -p testType={6}  -rff {0} -r 5 -i {1} -wi {2} -t {3} -bm thrpt" .format(file_path, iterations, warmup_iterations, threads, implementations, benchmarks, test_type)
+		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+		while True:
+			output = process.stdout.readline()
+			if output == '' and process.poll() is not None:
+				break
+			if output:
+				print output.strip()
 
-	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	while True:
-		output = process.stdout.readline()
-		if output == '' and process.poll() is not None:
-			break
-		if output:
-			print output.strip()
-	
 
 
