8f2751ac1b1e36e32e02f5a4cb7f370355d1bf5a
Michel Tosu <michel@tosu.se>
2020-04-06 10:05:01 +0200

    sort results before plotting

--- plotter.py
+++ plotter.py
@@ -9,10 +9,11 @@ plt.style.use('ggplot')
 
 
 for size in [128, 1024,8192,16384,131072,1048576]:
-    for test in ['iterate', 'iteratepure', 'update', 'even']:
+    for test in ['iterate', 'update', 'even']:
         ax = plt.gca()
         selected = df[(df['Param: size'] == size) & (df['Param: testType'] == test)]
-        selected.groupby(['Param: impl', 'Param: testType']).plot(kind='line',x='Threads',y='Score',ax=ax)
+        sorted = selected.sort_values(['Threads'])
+        sorted.groupby(['Param: impl', 'Param: testType']).plot(kind='line',x='Threads',y='Score',ax=ax)
         plt.savefig(test + "_" + str(size)+".png")
         plt.clf()
 
