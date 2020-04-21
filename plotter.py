499d3cfdcd4d1eb35896e24e22f435069810c171
Tobias Bjerking <tobias.bjerking@gmail.com>
2020-04-21 10:27:34 +0200

    calculate the average when tests result in multiple values

--- plotter.py
+++ plotter.py
@@ -3,18 +3,17 @@ from matplotlib import pyplot as plt
 import sys
 
 #read file
-df = pd.read_csv(sys.argv[1])
+df = pd.read_csv(sys.argv[1], sep=',')
 
 plt.style.use('ggplot')
 
-
 for size in [128, 1024,8192,16384,131072,1048576]:
     for test in ['iterate', 'update', 'even']:
-        for impl in ['ONLINE_ADAPTIVE_LIST', 'ONLINE_ADAPTIVE_MAP', 'WRAPPED_MAP', 'WRAPPED_LIST']:
+        for impl in ['ADAPTIVE', 'WRAPPED_MAP', 'WRAPPED_LIST']:
             ax = plt.gca()
             selected = df[(df['Param: size'] == size) & (df['Param: testType'] == test) & (df['Param: impl'] == impl)]
-            sorted = selected.sort_values(['Threads'])
-            sorted.plot(kind='line',x='Threads',y='Score',ax=ax,label=impl)
+            grouped = selected.groupby('Threads').mean()
+            grouped.plot(kind='line',y='Score',ax=ax,label=impl)
         plt.legend(loc='upper left')
         plt.savefig(test + "_" + str(size)+".png")
         plt.clf()
